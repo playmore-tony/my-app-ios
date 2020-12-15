8a4c2def1414a7a79cac8deed8509e715344a6d4
Tony Sit <tony1994217@gmail.com>
2020-12-15 15:22:11 +0800

    -- intinal commit

---
+++ Shared/myappApp.swift
@@ -0,0 +1,17 @@
+//
+//  myappApp.swift
+//  Shared
+//
+//  Created by Tony Sit on 15/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct myappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
